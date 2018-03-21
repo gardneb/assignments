1f3d0ccac2bec2fe2ef55d94add8e7dc8e36589a
bonnie.gardner <bonnie.gardner@simmons.edu>
2018-03-20 20:49:46 -0400

    In-class assignment for March 20

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,29 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+let my_name = "Bonnie";
+let your_name = "Kyle";
+
+if (my_name === "Ben") {
+  renderOutput("That's Ben!");
+} else if (my_name === "Bonnie") {
+  renderOutput("Yep, that's me!");
+}
+else {
+  renderOutput("I must be someone else");
+}
+
+function helloWorld() {
+  renderOutput("Hello World!");
+}
+
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+
+helloName("Bonnie");
+
+function todaysHours(days_hours) {
+  renderOutput(days_hours);
+}
+
+hours.forEach(todaysHours);
