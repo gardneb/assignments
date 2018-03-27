796e2fb17a07cc5d56add5afea826c0b2c0a8baf
bonnie.gardner <bonnie.gardner@simmons.edu>
2018-03-27 19:39:12 -0400

    Out of Class Assignment Week 6

---
+++ week-6/out-of-class/js/data.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,3 +4,20 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
+
+function days_hours(data) {
+  let separate = data.split(": ");
+  let day = separate[0];
+  let times = separate[1];
+  if (times === "closed") {
+    renderOutput("On " + day + " the library is " + times);
+ } else {
+    renderOutput("On " + day + " the hours are " + times);
+  }
+}
+
+function dailyHours(hours) {
+  hours.forEach(days_hours);
+}
+
+dailyHours(hours);
